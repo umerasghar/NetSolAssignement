0887e2406b6a6067f76d0801256a41be06efa490
Umer Asghar <asgharumer219@gmail.com>
2022-03-21 20:58:26 +0500

    NetSol Assignemnt

---
+++ NetSolProject/Assets/Scripts/HeliComponents.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+public class HeliComponents
+{
+    public Rigidbody heliRigidBody;
+    public float forward_force=0;
+    public float rotation_force=0;
+    public float sideways_force = 0;
+    public float ascend_force = 0;
+    public Transform projectileShoot;
+
+}
---
+++ NetSolProject/Assets/Scripts/HeliController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeliController : MonoBehaviour
+{
+    public HeliComponents currentHeli;
+    float forward=0, sideways=0, ascend=0, rotateFactor=0;
+    bool isShoot = false;
+    // Update is called once per frame
+    void Update()
+    {
+        forward = Input.GetAxis("Vertical")*currentHeli.forward_force*Time.deltaTime;
+        sideways = Input.GetAxis("Horizontal")*currentHeli.sideways_force*Time.deltaTime;
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            rotateFactor = currentHeli.rotation_force*Time.deltaTime;
+        }
+        if (Input.GetKeyUp(KeyCode.R))
+        {
+            rotateFactor = 0f;
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ascend = currentHeli.ascend_force * Time.deltaTime;
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            ascend = 0f;
+        }
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+            isShoot = true;
+        }
+    }
+    private void FixedUpdate()
+    {
+        currentHeli.heliRigidBody.AddForce(new Vector3(sideways, ascend, forward));
+        currentHeli.heliRigidBody.AddTorque(new Vector3(0f, rotateFactor, 0f));
+        RaycastHit hit;
+       if(Physics.Raycast(currentHeli.projectileShoot.localPosition, currentHeli.projectileShoot.forward,out hit)&&isShoot)
+        {
+            if (hit.collider.gameObject.tag.Equals("Enemy"))
+            {
+                Destroy(hit.collider.gameObject);
+            }
+            isShoot = false;
+        }
+    }
+
+}
